838007f1b63f3246cd8f81d1475a2474cf38cc41
AlisaBugaeva <92516286+AlisaBugaeva@users.noreply.github.com>
2022-03-07 17:32:29 +0300

    faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,27 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool hasNum = false;
+    int i = 0;
+    int count = 0;
+
+    while (str[i] ) {
+        if (str[i] != ' ') {
+            if (!hasNum) {
+                if (isdigit(str[i])) {
+                    hasNum = true;
+                }
+            }
+        }
+        else if (str[i] == ' ') {
+            if (!hasNum) {
+                count++;
+            }
+            else hasNum = false;
+        }
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
